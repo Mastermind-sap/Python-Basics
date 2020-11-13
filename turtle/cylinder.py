756a34315c437cf9bc2f12bc390b4e4e86592170
Mastermind-sap <66560136+Mastermind-sap@users.noreply.github.com>
2020-11-14 03:59:11 +0530

    Create cylinder.py

---
+++ turtle/cylinder.py
@@ -0,0 +1,18 @@
+from turtle import *
+import turtle
+i=1
+while(i==1):
+    turtle.shape('circle')
+    turtle.color("green")
+    turtle.width(2)
+    circle(120)
+    turtle.penup()
+    turtle.right(90)
+    turtle.forward(20)
+    turtle.right(-90)
+    i=1
+    turtle.shape('circle')
+    turtle.color("blue")
+    turtle.width(2)
+    turtle.pendown()
+    circle(140)
