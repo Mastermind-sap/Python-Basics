795bf8ee6cf47d38115e1a1b639c0ddf4fe538e0
Mastermind-sap <66560136+Mastermind-sap@users.noreply.github.com>
2020-11-14 04:01:33 +0530

    Create solar.py

---
+++ turtle/solar.py
@@ -0,0 +1,20 @@
+from turtle import *
+import turtle
+turtle.bgcolor("blue")
+turtle.width(5)
+begin_fill()
+turtle.color("orange","red")
+circle(100)
+end_fill()
+turtle.penup()
+turtle.right(90)
+turtle.forward(20)
+turtle.right(-90)
+i=1
+turtle.shape('circle')
+turtle.color("green")
+while(i==1):
+    circle(120)
+    turtle.width(2)
+    turtle.pendown()
+
