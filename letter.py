d6e419957cdae5409389417374930a2b5a4ac571
Mastermind-sap <66560136+Mastermind-sap@users.noreply.github.com>
2020-11-14 03:51:57 +0530

    Create letter.py

---
+++ letter.py
@@ -0,0 +1,16 @@
+try:
+    name=input("Enter a letter to check whether a vowel or consonant : ")
+    a=name.upper()
+except NameError:
+    if a=='A':
+        print "it is a vowel"
+    elif a=='E':
+        print "it is a vowel"
+    elif a=='I':
+        print "it is a vowel"
+    elif a=='O':
+        print "it is a vowel"
+    elif a=='U':
+        print "it is a vowel"
+    else:
+        print "it is a consonant"
